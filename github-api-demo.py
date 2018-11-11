InternationalUnderground/api-demo

commit a4ff4a86b02d9384d682ed39cbfcaee51b27a116

Better status code handling and documentation
The application now handles checking for the content-type and request
method earlier in the request processing, simplifying the route method
a bit and making it a bit clearer why we're returning the code we are.

We also now return 405 and 415 for invalid requests rather than 200
with a message in the response body. If we're expecting JSON, we should
enforce that.

github-api-demo.py
@@ -17,15 +17,19 @@ def get_yaml_var(key):
   config_var = settings['github'][key]
   return config_var
 
-def is_github_request(request):
+def request_verify(request):
+  if request.method is not 'POST':
+    abort(405)
+  if request.content_type is not 'application/json':
+    abort(415)
+  
+def webhook_verify(request):
   signature = request.headers['X-Hub-Signature'].split('=')
   key = get_config_var('GH_SECRET')
   body = request.data
   hashed = hmac.new(key.encode('utf-8'), body, hashlib.sha1)
   digest = hashed.hexdigest()
-  if hmac.compare_digest(signature[1], digest):
-    return True
-  else:
+  if hmac.compare_digest(signature[1], digest) is not True
     return abort(403)
 
 def prepare_issue(repo):
@@ -44,22 +48,16 @@ def hello():
 
 @app.route("/webhook/", methods=['POST','GET'])
 def post_comment_on_delete():
-  if request.method == 'POST':
-    if request.is_json:
-      hook = request.get_json(silent =True)
-      if is_github_request(request):
-        issue_body = prepare_issue(hook)
-      else:
-        abort(403)
-      if issue_body[1] == True:
-        notify_on_delete = get_yaml_var('notify_delete')
-        target_repo = g.get_repo(get_yaml_var('target_repo'))
-        target_repo.create_issue(title='Repository deleted', body="%s cc:%s" % (issue_body[0], notify_on_delete))
-        return str(issue_body[0])
-      else:
-        notify_else = get_yaml_var('notify_else')
-        return "%s cc: %s" % (issue_body[0], notify_else)
-    else:
-      return "Not a JSON object. Nothing to do!"
+  request_verify(request)
+  webhook_verify(request)
+  hook = request.get_json(silent =True)
+  if issue_body[1] == True:
+    notify_on_delete = get_yaml_var('notify_delete')
+    target_repo = g.get_repo(get_yaml_var('target_repo'))
+    target_repo.create_issue(title='Repository deleted', body="%s cc:%s" % (issue_body[0], notify_on_delete))
+    return str(issue_body[0])
   else:
-    return "Not a POST"
+    if notify_else is not None:
+      return "%s cc: %s" % (issue_body[0], notify_else)
+    else:
+      return "%s cc: %s" % (issue_body[0])

commit 376e596c4386d97512323ff45fe1cbc75b6a40b0

Prepares the issue (fixes bug introduced in a4ff4a8)
When we moved the status code and method checking out to their own
methods, we forgot to prepare the issue body elsewhere, causing 500 errors.

github-api-demo.py
@@ -53,6 +53,7 @@ def post_comment_on_delete():
   request_verify(request)
   webhook_verify(request)
   hook = request.get_json(silent =True)
+  issue_body = prepare_issue(hook)
   if issue_body[1] == True:
     notify_on_delete = get_yaml_var('notify_delete')
     target_repo = g.get_repo(get_yaml_var('target_repo'))